99a4cc962f46ad5e42cc658769a228732c409a3b
Wilian-Miranda <69541095+Wilian-Miranda@users.noreply.github.com>
2020-12-06 22:31:31 -0600

    Trabajando con presentación, Thymealft. Html 5 y CSS Bootstrap

--- Springdatajpasqlserver/src/main/java/com/unab/Springdatajpasqlserver/Web/ControllerPersona.java
+++ Springdatajpasqlserver/src/main/java/com/unab/Springdatajpasqlserver/Web/ControllerPersona.java
@@ -2,9 +2,11 @@ package com.unab.Springdatajpasqlserver.Web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.unab.Springdatajpasqlserver.DAO.PersonaDAO;
+import com.unab.Springdatajpasqlserver.Entidades.Persona;
 
 @Controller
 public class ControllerPersona {
@@ -13,17 +15,18 @@ public class ControllerPersona {
 	private PersonaDAO perDAO;
 	
 	@GetMapping("/")
-	public String Inicio() {
+	public String Inicio(Model M) {
 		
 		var MostrarPersonas = perDAO.findAll();  
 		
-		for (var iteracion: MostrarPersonas) {
-			System.out.println("id = "+iteracion.getId_persona() + "   Nombre = " + iteracion.getNombre()
-			+ "   Email = " + iteracion.getEmail() + "   Telefono = " + iteracion.getTelefono());
-
-		}
+		M.addAttribute("persona",MostrarPersonas );
 		
 		return "Index";
+	}
+	
+	@GetMapping("/agregar")
+	public String Agregar(Persona Persona) {
+		return "agregar";
 	}
 
 }
